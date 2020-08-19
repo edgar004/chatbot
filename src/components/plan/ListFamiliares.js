fc2c5f1c73cf25bb28c7a0198c2f98a6c330b30b
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2020-08-19 15:13:31 -0400

    Updates

--- src/components/plan/ListFamiliares.js
+++
@@ -1,9 +0,0 @@
-import React, { memo } from "react";
-
-export const ListFamiliares = memo(({ title }) => {
-  return (
-    <div className="card">
-      <h6 className="card-header">{title}</h6>
-    </div>
-  );
-});
